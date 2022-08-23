3185efed1f32bb398390fa92af9d86564107fb3e
praveenfzs <gpraveen2802@gmail.com>
2022-08-23 08:36:37 +0530

    About us page design

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    // Hamburger 
+    $(".hamburger").click(function(){
+      $(this).toggleClass("is-active");
+      $(".menu_outer_wrap").toggleClass("active");
+    });
+
+
+});
+  
