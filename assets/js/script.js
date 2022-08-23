aa0e223e6e8230aa6b45c134170b4b1d7d6f31bd
Praveenkumar <praveenkumar.g@osiztechnologies.in>
2022-08-23 12:15:39 +0530

    counter script added

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,17 @@ $(document).ready(function(){
       $(".menu_outer_wrap").toggleClass("active");
     });
 
+    $('.counters').each(function () {
+      $(this).prop('Counter',0).animate({
+          Counter: $(this).text()
+      }, {
+          duration: 4000,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(Math.ceil(now));
+          }
+      });
+  });
 
 });
   
